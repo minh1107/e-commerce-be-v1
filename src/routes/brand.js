4326e59926bd1bc00346a4609a7389b41b1b75f3
minh1107 <minhdepzaiahihi@gmail.com>
2024-01-10 14:14:27 +0700

    fixbug

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -2,7 +2,7 @@ const routers = require('express').Router()
 const controller = require('../controllers/brand')
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
-
+// router
 routers.post('/', [verifyAccessToken, isAdmin] ,controller.createBrand)
 routers.get('/', [verifyAccessToken, isAdmin] ,controller.getBrand)
 routers.put('/:bid', [verifyAccessToken, isAdmin] ,controller.updateBrand)
