61756d3ca5438130163082ab21923b4bd2972ab3
minh1107 <minhdepzaiahihi@gmail.com>
2024-01-10 14:38:44 +0700

    up

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dbConnect = require('./src/config/dbConnect')
 const initRoutes = require('./src/routes')
 const cookie = require('cookie-parser')
 const cors = require('cors')
-const { createProxyMiddleware } = require('http-proxy-middleware')
 require('dotenv').config()
 
 const app = express()
@@ -16,12 +15,6 @@ app.use(cors({
     methods: ['POST', 'PUT', 'GET', 'DELETE', 'PATCH'],
     credentials: true,
 }))
-app.use(
-    createProxyMiddleware({
-      target: process.env.CLIENT_URL,
-      changeOrigin: true,
-    })
-  );
 dbConnect()
 initRoutes(app)
 
