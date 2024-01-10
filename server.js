12ddf05f2ed175270efcd5772f46a7ec2ef417d7
minh1107 <minhdepzaiahihi@gmail.com>
2024-01-10 16:07:55 +0700

    update1

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookie())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://e-commerce-fe-v1.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     next();
   });
   
