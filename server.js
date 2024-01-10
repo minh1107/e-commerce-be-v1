b56c93c4cbc927611e61cd2a6bd540a2d27b391a
minh1107 <minhdepzaiahihi@gmail.com>
2024-01-10 16:50:05 +0700

    update

--- server.js
+++ server.js
@@ -10,28 +10,29 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   next();
+// });
 
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL || "https://e-commerce-fe-v1.vercel.app",
-//     methods: ["POST", "PUT", "GET", "DELETE", "PATCH"],
-//     credentials: true,
-//   })
-// );
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://e-commerce-fe-v1.vercel.app",
+    methods: ["POST", "PUT", "GET", "DELETE", "PATCH"],
+    credentials: true,
+    headers: ["Content-Type"],
+  })
+);
+app.options('*', cors())
 dbConnect();
 initRoutes(app);
 
